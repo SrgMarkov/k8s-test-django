af2d2c630035ca2d489d6608865ebc3b0cfc351c
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2024-03-28 13:05:50 +0400

    move backend files to parent folder

--- dev-yc-sirius/edu-jovial-mclean/backend/src/webapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import redirect
-from django.contrib import admin
-from django.urls import path
-
-
-urlpatterns = [
-    path('', lambda request: redirect('/admin/')),
-    path('admin/', admin.site.urls),
-]
